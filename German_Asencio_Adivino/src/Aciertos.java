653af3230c5e25d19010158328218d329e5c1b55
gaa <gaa@192.168.0.27>
2019-10-19 18:14:00 +0200

    Initial commit in cambio branch

--- German_Asencio_Adivino/src/Aciertos.java
+++ German_Asencio_Adivino/src/Aciertos.java
@@ -4,7 +4,9 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int min = 10;
+        int max = 50;
+        int x = (int)(Math.random() * ((max - min) + 1)) + min;
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
